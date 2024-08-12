9a9987321350f1c4725bbd1902049b4f276611f3
who <283184164@qq.com>
2024-08-12 21:26:23 +0800

    get input stream form url handle input stream print or write to output stream

--- src/main/java/cn/haoziy/http/HttpUtil.java
+++ src/main/java/cn/haoziy/http/HttpUtil.java
@@ -2,6 +2,7 @@ package cn.haoziy.http;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.Map;
 
@@ -9,6 +10,11 @@ public class HttpUtil {
     protected InputStream inputStream;
     protected final Integer CONNECT_TIMEOUT = 5000;
     protected final Integer READ_TIMEOUT = 5000;
+    protected final String DEFAULT_CHARSET = "UTF-8";
+    protected final byte[] BUFFER = new byte[(1 << 12) * 5];
+    protected final StringBuilder responseBuilder = new StringBuilder();
+    protected final Proxy defaultProxy = Proxy.NO_PROXY;
+
 
 
     /**
@@ -31,18 +37,17 @@ public class HttpUtil {
     }
 
     /**
-     *
-     * @param strUrl string url
+     * @param strUrl         string url
      * @param connectTimeout connect timeout
-     * @param readTimeout read timeout
+     * @param readTimeout   read timeout
      * @return input stream of file
      */
-    public HttpUtil httpGetInputStream(String strUrl,int connectTimeout,int readTimeout) {
+    public HttpUtil httpGetInputStream(String strUrl, int connectTimeout, int readTimeout) {
         URL url = constructURL(strUrl);
         try {
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setConnectTimeout(connectTimeout<=0?CONNECT_TIMEOUT:connectTimeout);
-            connection.setReadTimeout(readTimeout<=0?READ_TIMEOUT:readTimeout);
+            connection.setConnectTimeout(connectTimeout <= 0 ? CONNECT_TIMEOUT : connectTimeout);
+            connection.setReadTimeout(readTimeout <= 0 ? READ_TIMEOUT : readTimeout);
             this.inputStream = connection.getInputStream();
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
@@ -51,29 +56,21 @@ public class HttpUtil {
     }
 
     /**
-     * download file from url with headers
-     *
-     * @param strUrl  string url
-     * @param headers headers
-     * @return input stream of file
+     * print input stream
+     * @return response string
+     * @throws IOException if io exception occurs
      */
-    public HttpUtil httpGetInputStream(String strUrl, Map<String, String> headers) {
-        URL url = constructURL(strUrl);
-        try {
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            if (headers != null) {
-                for (Map.Entry<String, String> entry : headers.entrySet()) {
-                    connection.setRequestProperty(entry.getKey(), entry.getValue());
-                }
-            }
-            connection.setConnectTimeout(CONNECT_TIMEOUT);
-            connection.setReadTimeout(READ_TIMEOUT);
-            this.inputStream = connection.getInputStream();
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
+    public String printInputStream() throws IOException {
+        FileInputStream fis = (FileInputStream) inputStream;
+        int len;
+        while ((len = fis.read(BUFFER)) > 0) {
+            System.out.println();
+            responseBuilder.append(new String(BUFFER, 0, len, Charset.forName(DEFAULT_CHARSET)));
         }
-        return this;
-    }
+        return responseBuilder.toString();
+}
+
+
 
 
     /**
@@ -82,8 +79,14 @@ public class HttpUtil {
      * @param url url
      * @return file name
      */
-    private static String getFileName(URL url) {
+    private  String getFileName(URL url) {
         Path path = Path.of(url.getPath());
+        try {
+           HttpURLConnection connection = (HttpURLConnection) url.openConnection(defaultProxy);
+            System.out.println(connection.getContentType());
+        }catch (IOException e){
+            return url.getPath();
+        }
         return path.getFileName().toString();
     }
 
@@ -123,18 +126,22 @@ public class HttpUtil {
         return new FileOutputStream(path);
     }
 
-    public static void main(String[] args) {
-        URL url = HttpUtil.constructURL("https://admin.chinesetest.cn/userFace/2023/2023-11-26/828636/43414768/d9323ebe50bf4b678e1b27570dfd9f40.jpg");
-        HttpUtil httpUtil = new HttpUtil();
 
-        try {
-            System.out.println(httpUtil.httpGetInputStream(url.toString()).inputStream.transferTo(new FileOutputStream(System.getenv("USERPROFILE")+"/"+(getFileName(url)))));
-            System.out.println(System.getenv("USERPROFILE")+"\\"+(getFileName(url)).replaceAll("\\\\","/"));
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            System.out.println(connection.getContentType());
-            System.out.println(connection.getRequestMethod());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+    public static void main(String[] args) throws Exception {
+//        URL url = HttpUtil.constructURL("https://admin.chinesetest.cn/userFace/2023/2023-11-26/828636/43414768/d9323ebe50bf4b678e1b27570dfd9f40.jpg");
+//        HttpUtil httpUtil = new HttpUtil();
+//
+//        try {
+//            System.out.println(httpUtil.httpGetInputStream(url.toString()).inputStream.transferTo(constructOutputStream(System.getenv("USERPROFILE") + "\\" + (getFileName(url)).replaceAll("\\\\", "/"))));
+//            System.out.println(System.getenv("USERPROFILE") + "\\" + (getFileName(url)).replaceAll("\\\\", "/"));
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            System.out.println(connection.getContentType());
+//            System.out.println(connection.getRequestMethod());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        URL url = HttpUtil.constructURL("https://admin.chinesetest.cn/userFace/2023/2023-11-26/828636/43414768/d9323ebe50bf4b678e1b27570dfd9f40.jpg");
+        System.out.println( new HttpUtil().getFileName(url));
     }
 }
